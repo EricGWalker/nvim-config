dcb16f86d71d0f61753da0a896c771649cd507dd
Ericdarkblade <ezmoswalker@gmail.com>
2024-01-24 23:16:30 -0500

    idk what I'm doing

--- init.lua
+++ init.lua
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-require("eric")
-require("eric.remap")
-=======
 require('config')
->>>>>>> 08c3d4f (completely new refactor of neovim config using lazy.nvim as manager, trashing old work)
