3c52a0b2fe665c947a8ffbbe1a950c859955c146
Ericdarkblade <ezmoswalker@gmail.com>
2024-02-08 23:00:00 -0500

    tried to make previewer work

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+vim.g.mkdp_echo_preview_url = 1
+vim.g.mkdp_browser = '$BROWSER'
+
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+}
+
