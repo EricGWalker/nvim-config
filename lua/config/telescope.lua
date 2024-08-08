abe2bc320fbd775233b3ec9c636be512883bd32f
Ericdarkblade <ewalkerpriority@gmail.com>
2024-08-08 09:31:51 -0400

    added fd shortcut

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -18,12 +18,15 @@ local builtin = require('telescope.builtin')
 
 --Keymap
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fd', builtin.fd)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 vim.keymap.set('n', '<leader>fu', builtin.buffers)
 vim.keymap.set('n', '<leader>fa', builtin.builtin)
 vim.keymap.set('n', '<leader>fk', builtin.keymaps)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 vim.keymap.set('n', '<leader>pv', function() telescope.extensions.file_browser.file_browser({initial_mode = 'normal', path='%:p:h'}) end) -- file browser
+
+
 
 -- vim.keymap.set('n', '<leader>fd', fb_extension.folder_browser, {}) -- folder directory
 -- above is broken... I will learn one day how to get it to work
