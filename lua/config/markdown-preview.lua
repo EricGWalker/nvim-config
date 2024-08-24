cde1acd5fb0560d6883c3729c76f4a5e7818f4ec
Ericdarkblade <ewalkerpriority@gmail.com>
2024-08-24 16:37:13 -0400

    markdown still broken :(

---
+++ lua/config/markdown-preview.lua
@@ -0,0 +1,4 @@
+vim.g.mkdp_echo_preview_url = 1
+-- vim.g.mkdp_browser = '$BROWSER'
+vim.g.mkdp_filetypes = { "markdown" }
+vim.g.mkdp_open_ip = '9000'
--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,16 +1,14 @@
-vim.g.mkdp_echo_preview_url = 1
-vim.g.mkdp_browser = '$BROWSER'
-
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-        vim.g.mkdp_echo_preview_url = 1
-        vim.g.mkdp_open_ip = '5100'
-        vim.g.mkdp_open_to_the_world = 1
-    end,
     ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 }
 
+-- return {
+--     "iamcco/markdown-preview.nvim",
+--     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--     build = "cd app && yarn install",
+--     ft = { "markdown" },
+-- }
+--
